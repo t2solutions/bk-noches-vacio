7669c834a93276a045b002d0775f5d2e37626440
tthoms <tthoms@minterior.gov.cl>
2019-08-27 19:05:40 -0300

    actualizacion al 27-08 ejecutando select y FX login

--- index.js
+++ index.js
@@ -1,48 +1,97 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const app = express();
+const port = process.env.PORT
 
 const db = require('./queries')
 
-
 app.use(bodyParser.json())
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 )
 
 app.get('/test', (request, response) => {
-  var jsonSalida = {
-		code: 0,
-    status: 'OK',
-    codigos: [1, 2, 3]
-  }	
-  
-  response.status(200);
-  response.json(jsonSalida);
-  
-  //response.json({ info: 'Node.js, Express, and Postgres API' })
+    var jsonSalida = {
+        code: 0,
+        status: 'OK',
+        codigos: [1, 2, 3]
+    }
+
+    response.status(200);
+    response.json(jsonSalida);
 })
 
-app.post('/login', (request, response) => {
+app.post('/login', async(request, response) => {
 
-  var username = request.body.username;
-  var pwd = request.body.password;
+    //var usernameIn = request.body.username;
+    //var pwdIn = request.body.password;
 
+    let loginInvoke = await db.doLogin2(request);
+    let doGetPersonalInformation;
+    if (loginInvoke.status == 200) {
+        doGetPersonalInformation = await db.doGetPersonalInformation(loginInvoke.data.idUsuario);
+    } else {
+        doGetPersonalInformation = loginInvoke;
+    }
 
+    //var jsonSalida = {
+    //   code: 0,
+    //  status: 'OK',
+    //  username: username
+    //}
 
-  var jsonSalida = {
-		code: 0,
-    status: 'OK',
-    username: username
-  }	
-  response.json(jsonSalida);
-  response.status(200);
+    //response.json(loginInvoke);
+    response.status(doGetPersonalInformation.status);
+    response.json(doGetPersonalInformation);
+
 })
+
+app.get('/combos/especie', async(request, response) => {
+    let doGetEspecies = await db.doGetEspecies();
+    response.status(doGetEspecies.status);
+    response.json();
+
+})
+
+app.get('/combos/zona', async(request, response) => {
+    let doGetZonas = await db.doGetZonas();
+    response.status(doGetZonas.status);
+    response.json(doGetZonas);
+})
+
+app.get('/combos/nivel', async(request, response) => {
+    let doGetNivel;
+
+
+    response.json(doGetNivel);
+    response.status(200);
+})
+
+app.get('/combos/subnivel', async(request, response) => {
+    let doGetSubnivel;
+
+    response.json(doGetSubnivel);
+    response.status(200);
+})
+
+app.get('/combos/rol', async(request, response) => {
+    let doGetRol;
+
+    response.json(doGetRol);
+    response.status(200);
+})
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
-  console.log('App running on port '+port+' ')
+    console.log('App running on port ' + port + ' ')
 })
-
--- queries.js
+++ queries.js
@@ -1,31 +1,96 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
 const options = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     password: process.env.DB_PASS,
     database: process.env.DB_DBNAME,
     multipleStatements: true
 };
 const mysqlConnection = mysql.createPool(options);
 
-const doCalculo = (request, response) => {
-    const querySp = "CALL SP(?);"
-    mysqlConnection.getConnection(
-        function(err, connection) {
+async function doLogin2(request) {
+    let querySp = "SELECT nochesvacio.FU_Login(?, ?) AS idUsuario;"
+    var usr = request.body.username;
+    var pwd = request.body.password;
+    try {
+        var result = await mysqlConnection.query(querySp, [usr, pwd]);
+    } catch (err) {
+        //next(err);
+        return result = { 'status': 500, 'message': err.sqlMessage };
+    }
 
+    if (result[0].length > 0) {
+        if (result[0][0].idUsuario == '0') {
+            return result = { 'status': 401, 'message': 'Usuario y/o contraseña incorrectos' };
+        } else {
+            return result = { 'status': 200, 'message': 'OK', 'data': result[0][0] };
         }
-    );
+
+    }
+}
+
+async function doGetPersonalInformation(userId) {
+    let querySp = "SELECT id_usuario AS idUsuario, rut, nombre, ap_pat AS apPat, ap_mat AS apMat, user AS usr, fono, id_tipousuario AS idTipoUsuario FROM usuario WHERE id_usuario = ?;"
+    try {
+        var result = await mysqlConnection.query(querySp, [userId]);
+        if (result[0].length > 0) {
+            return result = { 'status': 200, 'message': 'OK', 'data': result[0] };
+        } else {
+            return result = { 'status': 404, 'message': 'Datos no encontrados' };
+        }
+
+    } catch (err) {
+        return result = { 'status': 500, 'message': err.sqlMessage };
+    }
+
 
 }
 
+async function doGetEspecies() {
+    let querySp = "SELECT id_especie AS idEspecie, nombre FROM especie ORDER BY id_especie ASC;"
+    try {
+        var result = await mysqlConnection.query(querySp);
+        if (result[0].length > 0) {
+            return result = { 'status': 200, 'message': 'OK', 'data': result[0] };
+        } else {
+            return result = { 'status': 404, 'message': 'Datos no encontrados' };
+        }
 
-const doLogin = (request, response) => {
+    } catch (err) {
+        return result = { 'status': 500, 'message': err.sqlMessage };
+    }
+}
+
+async function doGetZonas() {
+    let querySp = "SELECT id_zona AS idZona, descripción AS descripcion, id_especie as idEspecie FROM Zona ORDER BY id_zona ASC;"
+    try {
+        var result = await mysqlConnection.query(querySp);
+        if (result[0].length > 0) {
+            return result = { 'status': 200, 'message': 'OK', 'data': result[0] };
+        } else {
+            return result = { 'status': 404, 'message': 'Datos no encontrados' };
+        }
+
+    } catch (err) {
+        return result = { 'status': 500, 'message': err.sqlMessage };
+    }
+}
+
+
+
+const doCalculo = (request, response) => {
 
 
 }
 
 module.exports = {
     doCalculo,
-    doLogin
-}
+    doLogin2,
+    doGetPersonalInformation,
+    doGetEspecies,
+    doGetZonas,
+};
